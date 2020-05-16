9fc4b4f03f5163ec417d992f407605d02c2f1e9e
josephexequiel <52370507+josephexequiel@users.noreply.github.com>
2020-05-16 17:33:19 +0800

    Update audio_sort.sh

--- audio_sort.sh
+++ audio_sort.sh
@@ -23,6 +23,21 @@ function prepare_directory()
 	unset WORK_OUTPUT;
 }
 
+function prepare_revertdirectory()
+{
+	WORK_OUTPUT="$1"
+	if [[ -d "$WORK_OUTPUT/Revert" ]];
+	then
+		echo "Cleaning up Revert directory."
+		rm -rf "$WORK_OUTPUT/Revert"
+		mkdir -p "$WORK_OUTPUT/Revert"
+	else
+		echo "Creating Revert directory.";
+		mkdir -p "$WORK_OUTPUT/Revert"
+	fi;
+	unset WORK_OUTPUT;
+}
+
 function parse_directory()
 {
 	WORK_DIR="$1"
@@ -65,4 +80,49 @@ function parse_directory()
 	unset WORK_DIR AUDIO_FILE FULLPATH DBCOUNT AUDIO_COUNT;
 }
 
-parse_directory "$1";
+function revert_directory()
+{
+	WORK_DIR="$1"
+	if [[ -d "$WORK_DIR" ]];
+	then
+		AUDIO_COUNT=$(ls $WORK_DIR | grep ".ogg" | wc -l);
+		if [ $AUDIO_COUNT != '0' ];
+		then
+			prepare_revertdirectory "$WORK_DIR";
+			echo "Processing $AUDIO_COUNT files in $WORK_DIR. Please wait.";
+			for AUDIO_FILE in `ls $WORK_DIR | grep ".ogg"`;
+			do
+				FULLPATH="$WORK_DIR/$AUDIO_FILE";
+				NEWNAME=$(echo "$AUDIO_FILE" | awk -F '_' '{print $2}');
+				NEWFULLPATH="$WORK_DIR/Revert/$NEWNAME";
+				if cp "${FULLPATH}" "${NEWFULLPATH}";
+				then
+					echo "${NEWFULLPATH} added."
+				fi;
+			done;
+			echo "Process finished.";
+			exit 0;
+		else
+			echo "No audio file was found in $WORK_DIR";
+			exit 1;
+		fi;
+	else
+		echo "$WORK_DIR does not exist."
+		exit 1;
+	fi;
+	unset WORK_DIR AUDIO_FILE FULLPATH DBCOUNT AUDIO_COUNT;
+}
+
+OPTION1="$1";
+OPTION2="$2";
+
+if [ "$OPTION1" == 'revert' ];
+then
+	revert_directory "$OPTION2";
+elif [ "$OPTION1" == 'convert' ];
+then
+	parse_directory "$OPTION2";
+else
+	echo "Invalid option.";
+	exit 1;
+fi;
